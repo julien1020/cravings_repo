82a18edc406b65d6c804a2144014ee12faef4dfb
Julien Antony <julien.m.antony@gmail.com>
2022-08-30 22:31:55 +0400

    Forgot password layouts finished

---
+++ app/src/main/java/com/julien/cravings/DisplayPasswordActivity.kt
@@ -0,0 +1,11 @@
+package com.julien.cravings
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DisplayPasswordActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_display_password)
+    }
+}
